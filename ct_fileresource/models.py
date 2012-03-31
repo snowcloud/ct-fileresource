f785899370e883fdbdec8f6865a75bbfa562e7cc
Derek Hoy <derek@snowcloud.co.uk>
2012-03-31 13:12:28 +0100

    app files added.

---
+++ ct_fileresource/models.py
@@ -0,0 +1,12 @@
+# ct_fileresource/models.py
+
+from django.db import models
+
+# Create your models here.
+
+
+class FileResource(models.Model):
+	"""docstring for FileResource"""
+
+	pass
+		
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ct-fileresource',
+    version='0.1dev',
+    packages=['ct_fileresource'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.rst').read(),
+)
